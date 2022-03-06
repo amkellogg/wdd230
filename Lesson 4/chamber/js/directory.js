5629b3e11a7467139462fefa913d054dbc03101c
kas17001@byui.edu <kas17001@byui.edu>
2022-03-05 18:44:17 -0700

    edits

--- Lesson 4/chamber/js/directory.js
+++ Lesson 4/chamber/js/directory.js
@@ -3,51 +3,56 @@ const jsonData =
     "https://amkellogg.github.io/wdd230/Lesson%204/chamber/js/directory.json";
 console.log(jsonData);
 
-let business = {};
-
 fetch(jsonData)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
         console.table(jsonObject); // temporary checking for valid response and data parsing
-        business = jsonObject["business"];
-
-        // business.forEach(displayBus);
-        business.forEach(displayBus);
+        const business = jsonObject["business"];
+        windowSize();
+        business.forEach(populateCards);
+        business.forEach(populateList);
     });
 
-function displayBus(bus) {
-    // Create elements to add to the document
-    let card = document.createElement("section");
-    let img = document.createElement("img");
-    let busname = document.createElement("p");
-    let busloc = document.createElement("p");
-    let phone = document.createElement("p");
-    let weburl = document.createElement("a");
-
-    img.setAttribute("src", bus.imageurl);
-    console.log(bus.imageurl);
-    img.setAttribute("alt", `${bus.busname}`);
-    busname.textContent = `${bus.busname} `;
-    busloc.textContent = `${bus.location}`;
-    phone.textContent = `${bus.phone}`;
-    weburl.textContent = `${bus.weburl}`;
-    // Add/append the section(card) with the h2 element
-    card.appendChild(img);
-    card.appendChild(busname);
-    card.appendChild(busloc);
-    card.appendChild(phone);
-    card.appendChild(weburl);
-
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector("#cards").appendChild(card);
+function displayBus() {
+    document.querySelector("#cards").style.display = "flex";
+    document.querySelector("#list").style.display = "none";
 }
-// function displaybus() {}
 
-//Show as a list
+function displayList() {
+    document.querySelector("#list").style.display = "block";
+    document.querySelector("#cards").style.display = "none";
+}
 
-function displayList(bus) {
+//Buttons and window rezie
+// const display = (displayFunction) => {
+//     business.forEach(displayFunction);
+// };
+
+const cardBtn = document.querySelector("#allCards");
+cardBtn.addEventListener("click", (event) => {
+    displayBus();
+});
+
+const listBtn = document.querySelector("#allList");
+listBtn.addEventListener("click", (event) => {
+    displayList();
+});
+// listBtn.addEventListener("click", displayList);
+
+function windowSize(event) {
+    if (window.innerWidth > 800 && window.innerWidth < 1000) {
+        displayList();
+    } else {
+        displayBus();
+    }
+    console.log(window.innerWidth);
+}
+window.addEventListener("resize", windowSize);
+window.addEventListener("load", windowSize);
+
+function populateList(bus) {
     // Create elements to add to the document
     let list_row = document.createElement("tr");
 
@@ -71,32 +76,31 @@ function displayList(bus) {
     table.appendChild(busloc);
     table.appendChild(phone);
     table.appendChild(weburl);
+}
+
+function populateCards(bus) {
+    // Create elements to add to the document
+    let card = document.createElement("section");
+    card.id = "cardhover";
+    let img = document.createElement("img");
+    let busname = document.createElement("p");
+    let busloc = document.createElement("p");
+    let phone = document.createElement("p");
+    let weburl = document.createElement("a");
+
+    img.setAttribute("src", bus.imageurl);
+    img.setAttribute("alt", `${bus.busname}`);
+    busname.textContent = `${bus.busname} `;
+    busloc.textContent = `${bus.location}`;
+    phone.textContent = `${bus.phone}`;
+    weburl.textContent = `${bus.weburl}`;
+    // Add/append the section(card) with the h2 element
+    card.appendChild(img);
+    card.appendChild(busname);
+    card.appendChild(busloc);
+    card.appendChild(phone);
+    card.appendChild(weburl);
 
     // Add/append the existing HTML div with the cards class with the section(card)
-    // document.querySelector("#list").appendChild(table);
+    document.querySelector("#cards").appendChild(card);
 }
-
-//Buttons and window rezie
-// const display = (displayFunction) => {
-//     business.forEach(displayFunction);
-// };
-
-const cardBtn = document.querySelector("#allCards");
-cardBtn.addEventListener("click", displayBus);
-
-const listBtn = document.querySelector("#allList");
-listBtn.addEventListener("click", (event) => {
-    business.forEach(displayList);
-    console.log(listBtn);
-});
-// listBtn.addEventListener("click", displayList);
-
-function windowSize() {
-    if (window.innerWidth > 800 && window.innerWidth < 1000) {
-        displayBus();
-    } else {
-        displayList();
-    }
-}
-// window.addEventListener("resize", reportWindowSize);
-// window.addEventListener("load", reportWindowSize);
