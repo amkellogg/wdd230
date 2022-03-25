2fc35060d93ea140ee78faebea43836b6fe5b3d8
kas17001@byui.edu <kas17001@byui.edu>
2022-03-25 12:21:06 -0600

    new

--- Final/ak.js
+++ Final/ak.js
@@ -1,17 +1,12 @@
 const jsonData = "https://amkellogg.github.io/wdd230/Final/ak.json";
 console.log(jsonData);
 
-fetch(jsonData)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
-        const projects = jsonObject["projects"];
-        projects.forEach(populateCards);
-    });
+fetch(jsonData).then(function (jsonObject) {
+    const projects = jsonObject["projects"];
+    projects.forEach(populateCards);
+});
 
-function populateCards(project) {
+function populateCards(projects) {
     // Create elements to add to the document
     let card = document.createElement("section");
     card.id = "cardhover";
@@ -19,7 +14,7 @@ function populateCards(project) {
     let projectname = document.createElement("h2");
     let caption = document.createElement("p");
     let imgurl = document.createElement("a");
-    imgurl.id = "webpage";
+    imgurl.id = "imgurl";
 
     img.setAttribute("src", projects.image);
     img.setAttribute("alt", `${projects.projectname}`);
@@ -32,7 +27,7 @@ function populateCards(project) {
     card.appendChild(caption);
     card.appendChild(imgurl);
 
-    console.log(img);
+    console.log();
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector("#cards").appendChild(card);
 }
